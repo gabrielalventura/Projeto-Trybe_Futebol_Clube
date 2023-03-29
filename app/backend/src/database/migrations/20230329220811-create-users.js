5a6f884ec46d312c1e6e73ecf88c5df5e4e1a662
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-29 19:15:44 -0300

    Feat: define migration para users

--- app/backend/src/database/migrations/20230329220811-create-users.js
+++ app/backend/src/database/migrations/20230329220811-create-users.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   await queryInterface.createTable('users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    username: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    password: {
+      allowNull: false,
+      type:Sequelize.STRING,
+    },
+   });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
   }
 };
