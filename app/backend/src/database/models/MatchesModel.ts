8d06ef1b1d5ca6c1002950f9b64cb6c03a4b2730
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-29 20:26:53 -0300

    Feat: Define model de Matches

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,74 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './TeamsModel';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    field: 'home_team_id',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'home_team_goals',
+  },
+  awayTeamId: {
+    type: INTEGER,
+    field: 'away_team_id',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+    field: 'in_progress',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeamId' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeamId' });
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeamId', as: 'homeTeamId' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeamId', as: 'awayTeamId' });
+
+export default Matches;
