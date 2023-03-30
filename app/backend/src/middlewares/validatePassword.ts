cf60efef89d5f2908b47935c06c567c53d0ec555
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-30 18:56:46 -0300

    Fix: Sem validações requisito 8 está ok, observar validações para corrigir

--- app/backend/src/middlewares/validatePassword.ts
+++ app/backend/src/middlewares/validatePassword.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 
-const validatePassword = async (req: Request, res: Response, next: NextFunction) => {
+const validatePassword = (req: Request, res: Response, next: NextFunction) => {
   const { password } = req.body;
 
   if (password.length < 6) {
     return res.status(401).json({ message: 'Invalid email or password' });
   }
 
-  // if (!password) {
-  //   return res.status(400).json({ message: 'All the fields must be filled' });
-  // }
+  if (!password) {
+    return res.status(400).json({ message: 'All the fields must be filled' });
+  }
 
   return next();
 };
