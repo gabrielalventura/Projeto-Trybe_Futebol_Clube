5b0247f8e7201f6c4f892db16f3f6278ebe13575
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-30 15:51:51 -0300

    Feat: Adiciona função para obter um time a partir do id em TeamsService

--- app/backend/src/database/services/TeamsService.ts
+++ app/backend/src/database/services/TeamsService.ts
@@ -1,4 +1,5 @@
 import { ModelStatic } from 'sequelize';
+import ITeams from '../../interfaces/ITeams';
 import Teams from '../models/TeamsModel';
 
 class ServiceTeams {
@@ -8,8 +9,14 @@ class ServiceTeams {
     const teams = await this.model.findAll();
     return teams;
   }
+
+  async getById(): Promise<ITeams | null> {
+    const team = await this.model.findByPk();
+    return team;
+  }
 }
 
 export default ServiceTeams;
 
 // desenvolvido com auxilio do Breno Lavalle, Lígia Bicalho e Maria Luiza Suhadolnik e também da documentação disponivel em: https://sequelize.org/docs/v6/other-topics/typescript/#requesting-a-model-class;
+// null utilizado por exigencia do lint;
