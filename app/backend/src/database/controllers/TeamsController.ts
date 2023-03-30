481c235eb16cadfa6085c4c7028500cd8f610dce
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-30 15:54:31 -0300

    Feat: Adiciona função para obter um time a partir do id em ControllerTeams

--- app/backend/src/database/controllers/TeamsController.ts
+++ app/backend/src/database/controllers/TeamsController.ts
@@ -12,6 +12,11 @@ class ControllerTeams {
     const teams = await this.service.getAll();
     res.status(200).json(teams);
   }
+
+  async getById(_req: Request, res: Response): Promise<void> {
+    const team = await this.service.getById();
+    res.status(200).json(team);
+  }
 }
 
 export default ControllerTeams;
