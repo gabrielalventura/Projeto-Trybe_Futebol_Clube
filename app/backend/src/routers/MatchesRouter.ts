9e744459fa69741f13909a34058ea77d0aa61a8d
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-04 16:48:01 -0300

    Feat: Cria rota patch para finalizar partidas

--- app/backend/src/routers/MatchesRouter.ts
+++ app/backend/src/routers/MatchesRouter.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import ControllerMatches from '../database/controllers/MatchesController';
+import validateToken from '../middlewares/validateToken';
 
 const controller = new ControllerMatches();
 const matchesRouter = Router();
 
 matchesRouter.get('/', (req, res) => {
   controller.getAll(req, res);
+});
+
+matchesRouter.patch('/:id/finish', validateToken, (req, res) => {
+  controller.finishById(req, res);
 });
 
 export default matchesRouter;
