af138d564b010312d87f0659965dd1263cbf141b
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-06 11:19:48 -0300

    Fix: Corrige query de homeTeam

--- app/backend/src/database/utils/HomeQuery.ts
+++ app/backend/src/database/utils/HomeQuery.ts
@@ -1,14 +1,14 @@
 const homeLeaderBoard = `SELECT t.team_name AS name,
-COUNT(*) AS totalGames, 
-SUM(CASE WHEN m.home_team_goals > m.away.team.goals THEN 3 
-WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE 0 END) AS totalPoints,
-SUM(CASE WHEN m.home_team_goals > m.away_team_goals THEN 1 ELSE 0 END) AS totalVictories,
-SUM(CASE WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE 0 END) AS totalDraws,
-SUM(CASE WHEN m.home_team_goals < m.away_team_goals THEN 1 ELSE 0 END) AS totalLosses,
-SUM(m.home_team_goals) AS goalsFavor,
-SUM(m.away_team_goals) AS goalsOwn,
-SUM(m.home_team_goals  - m.away_team_goals) AS goalsBalance
-FROM teams AS t JOIN matches AS m ON m.home_team_id = t.id AND m.in_progress IS FALSE GROUP BY name
+  SUM(CASE WHEN m.home_team_goals > m.away_team_goals THEN 3
+  WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE 0 END) AS totalPoints,
+  COUNT(*) AS totalGames, SUM(IF(m.home_team_goals > m.away_team_goals, 1, 0)) AS totalVictories,
+  SUM(IF(m.home_team_goals = m.away_team_goals, 1, 0)) AS totalDraws,
+  SUM(IF(m.home_team_goals < m.away_team_goals, 1, 0)) AS totalLosses,
+  SUM(home_team_goals) AS goalsFavor,
+  SUM(away_team_goals) AS goalsOwn,
+  (SUM(home_team_goals) - SUM(away_team_goals)) AS goalsBalance
+  FROM teams AS t JOIN matches AS m ON m.home_team_id = t.id
+  AND m.in_progress IS FALSE GROUP BY name
 `;
 
 export default homeLeaderBoard;
