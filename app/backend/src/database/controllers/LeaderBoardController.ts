0529cb1804b71bc0a21474027e94bda9a5f896c4
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-04-06 09:35:39 -0300

    Feat: Cria metodo no controller para home LeaderBoard

---
+++ app/backend/src/database/controllers/LeaderBoardController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import ServiceLeaderBoard from '../services/LeaderBoardService';
+
+class LeaderBoardController {
+  service:ServiceLeaderBoard;
+
+  constructor() {
+    this.service = new ServiceLeaderBoard();
+  }
+
+  async getHome(_req: Request, res: Response) {
+    const homeBoard = await this.service.getHome();
+    return res.status(200).json(homeBoard);
+  }
+}
+
+export default LeaderBoardController;
